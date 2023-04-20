8eb867326494fb22399632f25b7acf5cbcd140b9
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-04-20 09:46:32 +0200

    Demo module 4 partie 1

--- app/src/main/java/com/example/myapptest/MainActivity.kt
+++ app/src/main/java/com/example/myapptest/MainActivity.kt
@@ -2,71 +2,39 @@ package com.example.myapptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        // Correction partie 1
-        // tpMoyennePartie1()
+        // Récuperer le champ Email
+        val edtEmail = findViewById<EditText>(R.id.edt_email)
+        println(edtEmail.text)
 
-        // Correction partie 2
-        tpMoyennePartie2()
-    }
+        // Text view
+        val tvExample = findViewById<TextView>(R.id.tv_example)
 
-    // J'appel ma fonction
-    fun tpMoyennePartie1 () {
-        // Déclarer des notes
-        var note1 : Float = 15.0f
-        var note2 : Float = 18.0f
+        tvExample.text = "Toto";
 
-        // Calculer la moyenne
-        var moyenne = (note1+note2) / 2.0f;
+        // Méthode 1 = Définir l'action d'un clique sur le boutton
+        val btnPizza = findViewById<Button>(R.id.btn_pizza)
 
-        // Afficher
-        println("ENI-DEMO : " + moyenne)
-    }
-
-    fun moyenneMatiere(notes : MutableList<Float>, nomMatiere : String) : Float{
-        val moyenne = notes.average().toFloat();
-
-        println("ENI-DEMO : La moyenne en " + nomMatiere + " -> " + moyenne)
-
-        // Retourner la moyenne de la matière
-        return moyenne;
-    }
-
-    fun tpMoyennePartie2(){
-        // la liste de toute les moyennes (toute les matières)
-        var moyennesMatieres = mutableListOf<Float>()
-
-        // moyenne Math
-        moyennesMatieres.add(moyenneMatiere(mutableListOf<Float>(10.0f, 0.5f, 18.3f), "Math"))
-
-        // moyenne Francais
-        moyennesMatieres.add(moyenneMatiere(mutableListOf<Float>(3.0f, 0.5f, 2.0f), "Français"))
-
-        // moyenne Sport
-        moyennesMatieres.add(moyenneMatiere(mutableListOf<Float>(8.0f, 0.5f, 15.0f), "Sport"))
-
-        // moyenne génèrale
-        // la somme de toute les matières / par le nombre de matière
-        val moyenneGenerale = moyennesMatieres.average().toFloat()
-
-        println("ENI-DEMO : La moyenne generale est : " + moyenneGenerale)
-        // Version 2 : A la mano niveau 1
-        /*
-        var sum = 0.0f
-        for (note in notesMath){
-            sum += note
+        btnPizza.setOnClickListener {
+            println("J'ai cliqué !")
         }
-        val moyenne = sum / notesMath.size
-        */
 
-        // Version 3 : A la mano niveau 2
-        // val moyenne = notesMath.sum() / notesMath.size;
+    }
+
+    // Méthode 2 : Le XML pointe sur cette méthode
+    fun OnClickButtonCrevette(view: View) {
+        val tvExample = findViewById<TextView>(R.id.tv_example)
+
+        tvExample.text = "Oups j'ai attrapé une intoxication alimentaire"
     }
 }
