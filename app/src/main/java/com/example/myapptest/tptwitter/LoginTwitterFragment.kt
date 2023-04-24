6591fe8db743bfbf7bcea86112c1df08a06569b2
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-04-24 16:53:39 +0200

    Correction TP twitter5

--- app/src/main/java/com/example/myapptest/tptwitter/LoginTwitterFragment.kt
+++ app/src/main/java/com/example/myapptest/tptwitter/LoginTwitterFragment.kt
@@ -1,11 +1,16 @@
 package com.example.myapptest.tptwitter
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.myapptest.R
+import com.example.myapptest.databinding.FragmentLoginTwitterBinding
+import com.example.myapptest.databinding.FragmentOneBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -23,6 +28,26 @@ class LoginTwitterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_twitter, container, false)
+        // Vue du fragment en version data binding dans une variable
+        val binding = DataBindingUtil.inflate<FragmentLoginTwitterBinding>(inflater, R.layout.fragment_login_twitter, container, false)
+
+        // Que faire lors du clique du bouton
+        binding.btnLogin.setOnClickListener {
+            // Si au moins un champs est vide
+            if (binding.edtEmail.text.isEmpty() || binding.edtPaswword.text.isEmpty()){
+                //j'affiche un message
+                binding.tvFormMessage.text = "Veuillez saisir tous les champs"
+                binding.tvFormMessage.setTextColor(Color.parseColor("#FF0000"))
+            }
+            else {
+                // j'affiche un message
+                binding.tvFormMessage.text = "Saisies correctes"
+                // naviguer de login fragment à list fragment
+                findNavController().navigate(R.id.action_loginTwitterFragment_to_listTwitterFragment)
+            }
+        }
+
+        // Obligatoire retourner le root de la vue version DataBinding sinon page blanche
+        return binding.root
     }
 }
--- app/src/main/java/com/example/myapptest/tptwitter/TPTwitterActivity.kt
+++ app/src/main/java/com/example/myapptest/tptwitter/TPTwitterActivity.kt
@@ -18,21 +18,6 @@ class TPTwitterActivity : AppCompatActivity() {
         attd = DataBindingUtil.setContentView<ActivityTptwitterBinding>(this,
             R.layout.activity_tptwitter
         )
-
-
     }
 
-    // Que faire lors du clique sur le bouton Connexion
-    fun onClickLogin(view: View) {
-        // Si au moins un champs est vide
-        if (attd.edtEmail.text.isEmpty() || attd.edtPaswword.text.isEmpty()){
-            //j'affiche un message
-            attd.tvFormMessage.text = "Veuillez saisir tous les champs"
-            attd.tvFormMessage.setTextColor(Color.parseColor("#FF0000"))
-        }
-        else {
-            // j'affiche un message
-            attd.tvFormMessage.text = "Saisies correctes"
-        }
-    }
 }
